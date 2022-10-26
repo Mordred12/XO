abc65b632a3b680cfa30bb38cef32d3c46268008
magas <magas@MORDRED>
2022-10-26 15:01:41 +0400

    Добавьте файлы проекта.

---
+++ XO3/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace XO3
+{
+    partial class XO
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.stg = new System.Windows.Forms.Button();
+            this.container = new System.Windows.Forms.PictureBox();
+            this.res = new System.Windows.Forms.Label();
+            this.crestic = new System.Windows.Forms.Label();
+            this.nolic = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.container)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // stg
+            // 
+            this.stg.Location = new System.Drawing.Point(12, 12);
+            this.stg.Name = "stg";
+            this.stg.Size = new System.Drawing.Size(75, 23);
+            this.stg.TabIndex = 0;
+            this.stg.Text = "Начать";
+            this.stg.UseVisualStyleBackColor = true;
+            this.stg.Click += new System.EventHandler(this.stg_Click);
+            // 
+            // container
+            // 
+            this.container.Location = new System.Drawing.Point(12, 41);
+            this.container.Name = "container";
+            this.container.Size = new System.Drawing.Size(420, 420);
+            this.container.TabIndex = 1;
+            this.container.TabStop = false;
+            this.container.Click += new System.EventHandler(this.container_Click);
+            // 
+            // res
+            // 
+            this.res.AutoSize = true;
+            this.res.Location = new System.Drawing.Point(370, 17);
+            this.res.Name = "res";
+            this.res.Size = new System.Drawing.Size(0, 13);
+            this.res.TabIndex = 2;
+            // 
+            // crestic
+            // 
+            this.crestic.AutoSize = true;
+            this.crestic.Location = new System.Drawing.Point(153, 22);
+            this.crestic.Name = "crestic";
+            this.crestic.Size = new System.Drawing.Size(17, 13);
+            this.crestic.TabIndex = 3;
+            this.crestic.Text = "X-";
+            // 
+            // nolic
+            // 
+            this.nolic.AutoSize = true;
+            this.nolic.Location = new System.Drawing.Point(207, 22);
+            this.nolic.Name = "nolic";
+            this.nolic.Size = new System.Drawing.Size(18, 13);
+            this.nolic.TabIndex = 4;
+            this.nolic.Text = "O-";
+            // 
+            // XO
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(452, 482);
+            this.Controls.Add(this.nolic);
+            this.Controls.Add(this.crestic);
+            this.Controls.Add(this.res);
+            this.Controls.Add(this.container);
+            this.Controls.Add(this.stg);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "XO";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "XO";
+            this.Load += new System.EventHandler(this.XO_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.container)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button stg;
+        private System.Windows.Forms.PictureBox container;
+        private System.Windows.Forms.Label res;
+        private System.Windows.Forms.Label crestic;
+        private System.Windows.Forms.Label nolic;
+    }
+}
+
---
+++ XO3/Form1.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Drawing;
+using System.Media;
+using System.Windows.Forms;
+
+
+namespace XO3
+{
+    public partial class XO : Form
+    {
+        public byte[] ban = new byte[9];
+        public bool turn = false;
+        public byte crest = 0;
+        public byte nol = 0;
+
+        public void Draw(ref byte cell, int x1 , int y1, int x2, int y2)
+        {
+            Graphics field = container.CreateGraphics();
+            Pen seraPen = new Pen(Color.FromArgb(255, 84, 84, 84), 5);
+            Pen redPen = new Pen(Color.FromArgb(255, 205, 180, 121), 5);
+            if (cell == 0)
+            {
+                if (turn == false)
+                {
+                    field.DrawLine(seraPen, x1, y1, x2, y2);
+                    field.DrawLine(seraPen, x1, y2, x2, y1);
+                    cell = 1;
+                }
+                else
+                {
+                    field.DrawEllipse(redPen, x1, y1, 120, 120);
+                    cell = 2;
+                }
+                if (turn)
+                {
+                    turn = false;
+                }
+                else
+                {
+                    turn = true;
+                }
+            }
+        }
+        public void Win(int x1, int y1, int x2, int y2)
+        {
+            Graphics field = container.CreateGraphics();
+            Pen blackPen = new Pen(Color.FromArgb(255, 128, 94, 73), 5);
+            field.DrawLine(blackPen, x1, y1, x2, y2);
+            End();
+            if (turn != false)
+            {
+                crest++;
+                crestic.Text = $"X-{crest}";
+                res.Text = "Win X";
+                res.ForeColor = Color.Blue;
+
+            }
+            else
+            {
+                nol++;
+                nolic.Text = $"O-{nol}";
+                res.Text = "Win O";
+                res.ForeColor = Color.Red;
+            }
+            SoundPlayer sound = new SoundPlayer("brain-fart.wav");
+            sound.Play();
+
+        }
+
+        public void Reb()
+        {
+
+            ban[0] = 0;
+            ban[1] = 0;
+            ban[2] = 0;
+            ban[3] = 0;
+            ban[4] = 0;
+            ban[5] = 0;
+            ban[6] = 0;
+            ban[7] = 0;
+            ban[8] = 0;
+            turn = false;
+            res.Text = "Ход X";
+            res.ForeColor = Color.Blue;
+            res.Font = new Font("Verdana", 10, FontStyle.Regular);
+        }
+        public void End()
+        {
+            ban[0] = 1;
+            ban[1] = 2;
+            ban[2] = 3;
+            ban[3] = 4;
+            ban[4] = 5;
+            ban[5] = 6;
+            ban[6] = 7;
+            ban[7] = 8;
+            ban[8] = 9;
+        }
+        public XO()
+        {
+            InitializeComponent();
+            
+            crestic.Font = new Font("Verdana", 10, FontStyle.Regular);
+            nolic.Font = new Font("Verdana", 10, FontStyle.Regular);
+
+
+        }
+
+        private void container_Click(object sender, EventArgs e)
+        {
+            Graphics field = container.CreateGraphics();
+            Pen blackPen = new Pen(Color.FromArgb(255, 128, 94, 73), 5);
+            Pen seraPen = new Pen(Color.FromArgb(255, 84, 84, 84), 5);
+            Pen redPen = new Pen(Color.FromArgb(255, 205, 180, 121), 5);
+            int x, y;
+            Point p = container.PointToClient(Cursor.Position);
+            x = p.X;
+            y = p.Y;
+            
+            if (x <= 140 && y <= 140)
+            {
+                Draw(ref ban[0], 10, 10, 130, 130);
+            }
+            if (x >= 140 && x <= 280 && y <= 140)
+            {
+                Draw(ref ban[1], 150, 10, 270, 130);
+            }
+            if (x >= 280 && x <= 420 && y <= 140)
+            {
+                Draw(ref ban[2], 290, 10, 410, 130);
+            }
+            
+            if (x >= 0 && x <= 140 && y >= 140 && y <= 280)
+            {
+                Draw(ref ban[3], 10, 150, 130, 270);
+            }
+            if (x >= 140 && x <= 280 && y >= 140 && y <= 280)
+            {
+                Draw(ref ban[4], 150, 150, 270, 270);
+
+            }
+            if (x >= 280 && x <= 420 && y >= 140 && y <= 280)
+            {
+                Draw(ref ban[5], 290, 150, 410, 270);
+            }
+            if (x >= 0 && x <= 140 && y >= 280 && y <= 420)
+            {
+                Draw(ref ban[6], 10, 290, 130, 410);
+            }
+            if (x >= 140 && x <= 280 && y >= 280 && y <= 420)
+            {
+                Draw(ref ban[7], 150, 290, 270, 410);
+            }
+            if (x >= 280 && x <= 420 && y >= 280 && y <= 420)
+            {
+                Draw(ref ban[8], 290, 290, 410, 410);
+                    
+            }
+            if (turn == false)
+            {
+                
+                res.Text = "Ход X";
+                res.ForeColor = Color.Blue;
+            }
+            else
+            {
+                res.Text = "Ход O";
+                res.ForeColor = Color.Red;
+            }
+
+            /*Первая линия горизонт*/
+            if (ban[0] == ban[1] && ban[1] == ban[2] && ban[0]!=0)
+            {
+                Win(10, 70, 410, 70);
+            }
+
+            /*Вторая линия горизонт*/
+            if (ban[3] == ban[4] && ban[4] == ban[5] && ban[3] != 0)
+            {
+                Win(10, 210, 410, 210);
+            }
+
+            /*Третья линия горизонт*/
+            if (ban[6] == ban[7] && ban[7] == ban[8] && ban[6] != 0)
+            {
+                Win(10, 350, 410, 350);
+            }
+
+            /*Первая линия вертикальная*/
+            if (ban[0] == ban[3] && ban[3] == ban[6] && ban[0] != 0)
+            {
+                Win(70, 10, 70, 410);
+            }
+
+            /*Вторая линия вертикальная*/
+            if (ban[1] == ban[4] && ban[4] == ban[7] && ban[1] != 0)
+            {
+                Win(210, 10, 210, 410);
+            }
+
+            /*Третья линия вертикальная*/
+            if (ban[2] == ban[5] && ban[5] == ban[8] && ban[2] != 0)
+            {
+                Win(350, 10, 350, 410);
+            }
+
+            /* / линия верх-левое до низ-право*/
+            if (ban[0] == ban[4] && ban[4] == ban[8] && ban[0] != 0)
+            {
+                Win(10, 10, 410, 410);
+            }
+
+            /* / линия верх-право до низ-лево*/
+            if (ban[2] == ban[4] && ban[4] == ban[6] && ban[2] != 0)
+            {
+                Win(410, 10, 10, 410);
+            }
+
+        }
+
+        private void stg_Click(object sender, EventArgs e)
+        {
+            
+            Graphics field = container.CreateGraphics();
+            field.Clear(Color.White);
+            Reb();
+            Pen blackPen = new Pen(Color.FromArgb(255, 128, 94, 73), 5); 
+            field.DrawLine(blackPen, 140, 0, 140, 420);
+            field.DrawLine(blackPen, 280, 0, 280, 420);
+            field.DrawLine(blackPen, 0, 140, 420, 140);
+            field.DrawLine(blackPen, 0, 280, 420, 280);
+            SoundPlayer sound = new SoundPlayer("nachalo.wav");
+            sound.Play();
+
+        }
+
+        private void XO_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
